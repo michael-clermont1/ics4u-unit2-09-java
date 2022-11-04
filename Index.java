5d9c472314d04dcf70cffd472a5e89917d4ab4f1
Michael Clermont <Michael.Clermont@mths.ca>
2022-11-04 08:57:47 -0400

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,94 @@
+/**
+ * This is the standard Main() class starting point.
+ *
+ * @author  Michael Clermont
+ * @version 1.0
+ * @since   2020-10-04
+ */
+
+final class Index {
+    /**
+     * Constant Variable Status.
+     *
+     */
+    private static final String STATUS = "Status:";
+
+    /**
+     * Constant Variable MagicNumber10.
+     *
+     */
+    private static final int MAGIC10 = 10;
+
+    /**
+     * Constant Variable MagicNumber15.
+     *
+     */
+    private static final int MAGIC15 = 15;
+
+    /**
+     * Constant Variable MagicNumber1.
+     *
+     */
+    private static final int MAGIC1 = 1;
+
+    /**
+     * Constant Variable Speed.
+     *
+     */
+    private static final String NEWSPEED = "New Speed: ";
+
+    /**
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this ever is called
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Index() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+     * The starting main() function.
+     *
+     * @param args No args will be used
+     */
+    public static void main(final String[] args) {
+        // use Stack class
+        System.out.println("Created Bmx bike.\nStatus:\n");
+        final Bike bike = new Bike("Red", 40);
+        System.out.println(STATUS);
+        bike.status();
+
+        System.out.println("Set the cadence to 10\n");
+        bike.provideAir(MAGIC10);
+        bike.status();
+
+        System.out.println("\nAccelerate by 15");
+        bike.provideAir(MAGIC15);
+        bike.status();
+
+        System.out.println("\nRinging bell.");
+        bike.ringBell();
+
+        // new Ferrari
+        System.out.println("Created a Truck.\nStatus:\n");
+        final Truck truck = new Truck("Grey", "HGC-3456F", 200);
+        truck.status();
+
+        System.out.println("Accelerating, 10 of power for ten seconds.");
+        truck.accelerate(MAGIC10, MAGIC10);
+        System.out.println(NEWSPEED + truck.getSpeed());
+
+        System.out.println("\nBreaking, 10 of power for 10 sec.");
+        truck.accelerate(MAGIC10, MAGIC10);
+        System.out.println(NEWSPEED + truck.getSpeed());
+
+        System.out.println("\nApplyed air pressure of 10: ");
+        truck.provideAir(MAGIC10);
+        System.out.println(NEWSPEED + truck.getSpeed());
+
+        System.out.println("\nDone.");
+    }
+}
